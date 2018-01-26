621cb15d6eb80f4fdffaf2f7822511a9cba1b303
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-01-27 02:21:15 +0700

    create sperm prototype

---
+++ Assets/Scripts/SpermMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpermMovement : MonoBehaviour {
+    public float speed = 0.5f;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        this.transform.position += Vector3.up * speed * Time.deltaTime;
+	}
+}
