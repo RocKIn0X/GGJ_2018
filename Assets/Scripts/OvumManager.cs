925ac81b4d4fc7fb77358b0b19ec639e13cec6b6
DESKTOP-ST8M5EU\rockin <dantai.l@ku.th>
2018-01-28 03:49:15 +0700

    random ovum side

---
+++ Assets/Scripts/OvumManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvumManager : MonoBehaviour {
+    public int numRand;
+
+    private OvumMovement ovum;
+
+    void Start()
+    {
+        numRand = Random.Range(0, 2);
+
+        transform.GetChild(numRand).gameObject.SetActive(true);
+        ovum = transform.GetChild(numRand).gameObject.GetComponent<OvumMovement>();
+        if (numRand == 0) {
+            ovum.setForce(2500f);
+        } else {
+            ovum.setForce(-2500f);
+        }
+    }
+}
---
+++ Assets/Scripts/OvumMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OvumMovement : MonoBehaviour {
+    public float force;
+
+    private Rigidbody rb;
+    private IEnumerator coroutine;
+
+	// Use this for initialization
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+        coroutine = ForceToOvum();
+        StartCoroutine(coroutine);
+	}
+
+    public void setForce (float f)
+    {
+        force = f;
+    }
+
+    IEnumerator ForceToOvum()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.3f);
+            rb.AddForce(force * Vector3.right * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "OvumCheck")
+        {
+            StopCoroutine(coroutine);
+        }    
+    }
+}
