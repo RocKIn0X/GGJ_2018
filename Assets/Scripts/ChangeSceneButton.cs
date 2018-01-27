ad1fded6554bf6c1d3fbf999c44c325e4ccf8f8d
= <->
2018-01-27 09:09:16 +0700

    create main-menu layout ( wait insert art )

---
+++ Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour {
+    [SerializeField]
+    private string targetSceneName;
+
+    private Button button;
+
+    //private void OnValidate()
+    //{
+    //    if (SceneManager.GetSceneByName(targetSceneName) == null)
+    //        Debug.LogError("scene name : " + targetSceneName + " is not found in scene management");
+    //}
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+
+}
