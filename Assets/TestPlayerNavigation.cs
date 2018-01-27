f8bc51a35ee585325149bd3c9b7cebf13d00446d
= <->
2018-01-27 12:27:42 +0700

    create test navigation system scene

---
+++ Assets/TestPlayerNavigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class TestPlayerNavigation : MonoBehaviour {
+    RaycastHit hitInfo = new RaycastHit();
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+                agent.destination = hitInfo.point;
+        }
+    }
+}
